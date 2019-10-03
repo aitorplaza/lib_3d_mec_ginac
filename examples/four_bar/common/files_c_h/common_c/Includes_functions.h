56ae524a7dfb87b539ca6f6bf2baef571985fa68
jabierros <jros@unavarra.es>
2019-10-03 14:03:51 +0200

    Added four_bar example

---
+++ examples/four_bar/common/files_c_h/common_c/Includes_functions.h
@@ -0,0 +1,47 @@
+/* ********************************************************************************************* */
+/* Coord q */
+#ifdef COORD_DEP
+
+#include "Phi.h"
+#include "Beta.h"
+
+#include "Phi_q.h"
+#include "dPhi_dq.h"
+
+#ifdef DDL
+	#include "Qgamma.h"
+	#include "MXdPhi_dqZero.h"
+#endif
+
+#ifdef DDQ
+	#include "Q.h"
+	#include "M.h"
+	#include "Gamma.h"
+#endif
+
+	#ifdef I3AL
+		#include "M.h"
+		#include "Q.h"
+		#include "Q_q.h"
+		#include "Q_dq.h"
+		#include "Gamma.h"
+	#endif
+
+#endif
+/* ********************************************************************************************* */
+/* ********************************************************************************************* */
+/* Coord z */
+#ifdef COORD_IND
+
+#include "Phi.h"
+#include "Beta.h"
+#include "Phi_q.h"
+#include "dPhi_dq.h"
+#include "Gamma.h"
+#include "Q.h"
+#include "M.h"
+#include "MQ.h"
+#include "Gamma.h"
+
+#endif
+/* ********************************************************************************************* */
